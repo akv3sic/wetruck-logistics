ded1c1592df4052382e736c947788f20560ac085
Ante Kvesić <antekvesic2@gmail.com>
2022-01-21 14:29:04 +0100

    Add form for a new question to a FAQs page

--- web-app/src/views/FAQs.vue
+++ web-app/src/views/FAQs.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <Header />
-        <v-container class="pa-16">
+        <v-container :class="{'pa-16': $vuetify.breakpoint.lgAndUp}">
             <HeadingWithImage title="Često postavljana pitanja" height="33vh"/>
             <v-row>
                 <!-- PITANJA I ODGOVORI -->
-                <v-col cols="7" class="mt-8">
+                <v-col cols="12" md="7" class="mt-8">
                       <v-expansion-panels class="pa-4">
                         <v-expansion-panel
                         v-for="(item,i) in 7"
@@ -13,7 +13,7 @@
                         class="my-2 py-2"
                         >
                         <v-expansion-panel-header>
-                            Pitanje se nalazi ovdje?
+                            Sed do eiusmod tempor ut labore?
                             <template v-slot:actions>
                                 <v-icon color="primary">
                                 $expand
@@ -46,7 +46,42 @@
                         </v-expansion-panel>
                     </v-expansion-panels>
                 </v-col>
-                <v-col cols="3">sa strane pitanje mi novo postavi</v-col>
+
+                <!--  Forma za postavljanje novih pitanja -->
+                <v-col cols="12" md="5" lg="4">
+                    <v-card class="mx-auto new-question-form">
+                        <v-card-text class="py-10">
+                            <h3 class="display-1 text-center">Novo pitanje?</h3>
+                            <p class="caption text-center py-2 secondary--text">Ukoliko niste pronašli što vas zanima, slobodno nam se obratite.</p>
+
+                            <v-form ref="loginForm" lazy-validation> 
+                                <v-row>
+                                    <!-- E-mail -->
+                                    <v-col cols="10" class="mx-auto">
+                                        <v-text-field v-model="question.email" :rules="emailRules" label="E-mail" required></v-text-field>
+                                    </v-col>
+                                    <!-- Ime -->
+                                    <v-col cols="10" class="mb-2 mx-auto">
+                                        <v-text-field v-model="question.name" label="Ime" counter></v-text-field>
+                                    </v-col>
+                                    <!-- Pitanje -->
+                                    <v-col cols="10" class="mb-2 mx-auto">
+                                            <v-textarea
+                                            v-model="question.question"
+                                            counter
+                                            label="Pitanje"
+                                        ></v-textarea>
+                                    </v-col>
+                                    <!-- Button -->
+                                    <v-col cols="10" class="mx-auto">
+                                        <v-btn block class="primary">Pošalji</v-btn>
+                                    </v-col> 
+                                </v-row>
+                            </v-form>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+
             </v-row>
         </v-container>
         
@@ -59,6 +94,27 @@ import HeadingWithImage from "@/components/layout/HeadingWithImage"
 
 export default {
     name: "FAQs",
-    components: { Header, HeadingWithImage }
+    components: { Header, HeadingWithImage },
+    data: () => ({
+        question: {
+            email: "",
+            name: "",
+            question: ""
+        },
+        emailRules: [
+            v => !!v || "Obavezno polje.",
+            v => /.+@.+\..+/.test(v) || "Unesite ispravnu e-mail adresu."
+        ],
+        rules: {
+            question: v => v.length <= 25 || 'Maksimalno 150 znakova'
+        },
+    })
+
 }
 </script>
+
+<style scoped>
+.new-question-form {
+    transform: translate(0px, -40px);
+}
+</style>
